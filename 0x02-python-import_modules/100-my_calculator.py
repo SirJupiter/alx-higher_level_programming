36ea97eb27017572f9f3b4b2ace5841169bf5d1f
SirJupiter <jupitertoby@gmail.com>
2024-01-04 17:14:48 +0100

    6. Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -12,18 +12,18 @@ def main():
     if arg_count != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    elif arg_count == 4:
-        if argv[2] == '+':
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        elif argv[2] == '-':
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        elif argv[2] == '*':
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-        elif argv[2] == '/':
-            print("{} / {} = {}".format(a, b, div(a, b)))
-        else:
-            print('Unknown operator. Available operators: +, -, * and /')
-            exit(1)
+
+    if argv[2] == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif argv[2] == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif argv[2] == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif argv[2] == '/':
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print('Unknown operator. Available operators: +, -, * and /')
+        exit(1)
 
 
 if __name__ == "__main__":
