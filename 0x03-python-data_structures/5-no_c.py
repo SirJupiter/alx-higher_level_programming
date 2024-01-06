ba23ef3ac3e59805546da9d0ef996d9fa71953db
SirJupiter <jupitertoby@gmail.com>
2024-01-06 18:11:58 +0100

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new = ""
-    new.join(chr for char in my_string if char.lower() not in {'c'})
-    return (new)
+    new = []
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new.append(char)
+    return ''.join(new)
