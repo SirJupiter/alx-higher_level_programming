9a5cd22b3991142f9657e6eb93eb8e596beed0d1
SirJupiter <jupitertoby@gmail.com>
2024-01-24 13:05:14 +0100

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""1-square.py
+
+Module contains a class Square that defines a square
+"""
+
+
+class Square:
+    """A class representing a square."""
+
+    def __init__(self, size):
+        self.__size = size  # private attribute denoted by double underscore
