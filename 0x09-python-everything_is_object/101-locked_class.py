3457e2a6149dbbc38052b9ff3eb26ab0d39462b9
SirJupiter <jupitertoby@gmail.com>
2024-02-01 00:07:26 +0100

    all tasks

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module contains LockedClass class"""
+
+
+class LockedClass:
+    """A class that can be locked and unlocked to prevent access."""
+
+    __slots__ = ["first_name"]
