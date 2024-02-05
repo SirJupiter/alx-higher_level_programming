ea881c28d3bde31a4b955bf237134d562c9a2284
SirJupiter <jupitertoby@gmail.com>
2024-02-05 18:44:26 +0100

    5. Geometry module

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module contains 'BaseGeometry' class"""
+
+
+class BaseGeometry:
+    """Class represents a base geometry for other geometrical shapes.
+    Class is empty.
+    """
+    pass
