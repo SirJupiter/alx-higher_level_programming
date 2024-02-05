0bec36b7a318729a1c237256742037f4c48e7c97
SirJupiter <jupitertoby@gmail.com>
2024-02-05 16:26:48 +0100

    0. Lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module contains a function"""
+
+
+def lookup(obj):
+    """This is the docstring for the lookup() function.
+    It takes an object as input and returns its class name.
+    """
+    return [item for item in dir(obj)]
