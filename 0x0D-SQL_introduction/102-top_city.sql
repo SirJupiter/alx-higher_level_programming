951d395020471d4db44ff4cbe09738ade5c1e2d4
SirJupiter <jupitertoby@gmail.com>
2024-03-13 18:55:10 +0100

    displays the max temperature of each state (ordered by State name).

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,8 @@
--- displays the average temperature by city ordered by temp
+-- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
 USE hbtn_0c_0;
 SELECT city, AVG(value) AS avg_temp
-FROM temperatures
-WHERE month in (7, 8)
-GROUP BY city
-ORDER BY avg_temp DESC
-LIMIT 3;
+	FROM temperatures
+	WHERE month in (7, 8)
+	GROUP BY city
+	ORDER BY avg_temp DESC
+	LIMIT 3;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,6 @@
+-- ddisplays the max temperature of each state (ordered by State name).
+USE hbtn_0c_0;
+SELECT state, MAX(value) AS max_temp
+	FROM temperatures
+	GROUP BY state
+	ORDER BY state ASC;
