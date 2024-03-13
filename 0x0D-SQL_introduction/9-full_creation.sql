ced6c18830e87844c292d93247617f60d975b04a
SirJupiter <jupitertoby@gmail.com>
2024-03-13 15:40:12 +0100

    9. Full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,7 +5,5 @@ CREATE IF NOT EXISTS TABLE second_table (
 	score INT
 );
 
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
-INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
-INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
-INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
+INSERT INTO second_table (id, name, score)
+VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
