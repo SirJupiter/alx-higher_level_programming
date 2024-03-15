1b1da147995c97d07aaacfe3050fee7369ef03c0
SirJupiter <jupitertoby@gmail.com>
2024-03-15 01:11:54 +0100

    7. Cities table

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS cities (
 	PRIMARY KEY(id),
 	id INT NOT NULL AUTO_INCREMENT,
 	state_id INT NOT NULL,
-	FOREIGN KEY(state_id) REFERENCES states(id)
+	FOREIGN KEY(state_id) REFERENCES states(id),
+	name VARCHAR(256) NOT NULL
 );
