5642cbec9b06643e04f1f00a74db2837249adbd9
SirJupiter <jupitertoby@gmail.com>
2024-04-08 20:58:53 +0100

    6. Loop to languages

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -7,9 +7,9 @@ if (!process.argv[2]) {
 
 const num = Math.floor(parseFloat(process.argv[2], 10));
 // Check if the input is a valid number
-if (isNaN(num) || Number.isNaN(num) === true) {
+if (isNaN(num)) {
   console.log('Not a number');
   return;
 }
 
-console.log(`My number: ${num}`);
+console.log('My number: ' + num);
