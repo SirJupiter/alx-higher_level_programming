af4da55dfbdb18924fcfb219cd1941e8c80a681b
SirJupiter <jupitertoby@gmail.com>
2024-04-08 20:47:49 +0100

    5. An Integer

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+if (!process.argv[2]) {
+  console.log('Not a number');
+  return;
+}
+
+const num = Math.floor(parseFloat(process.argv[2], 10));
+// Check if the input is a valid number
+if (isNaN(num)) {
+  console.log('Not a number');
+  return;
+}
+
+console.log(`My number: ${num}`);
