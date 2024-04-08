49b28089e7ed6e2aade8501c21a9d73684ccfd85
SirJupiter <jupitertoby@gmail.com>
2024-04-08 21:50:48 +0100

    8. Square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,14 +2,11 @@
 
 if (!process.argv[2] || isNaN(parseInt(process.argv[2]))) {
   console.log('Missing size');
-  return;
 }
 
 const size = parseInt(process.argv[2]);
 
-if (size < 1) {
-  return;
-} else {
+if (size > 0) {
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       process.stdout.write('X');
