344f7ba19e21cdd6f78ce098d276f3471b39a218
SirJupiter <jupitertoby@gmail.com>
2024-04-08 21:52:53 +0100

    8. Square

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,14 +2,12 @@
 
 if (!process.argv[2]) {
   console.log('Not a number');
-  return;
 }
 
 const num = Math.floor(parseFloat(process.argv[2], 10));
 // Check if the input is a valid number
 if (isNaN(num)) {
   console.log('Not a number');
-  return;
+} else {
+  console.log('My number: ' + num);
 }
-
-console.log('My number: ' + num);
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,13 +2,12 @@
 
 if (!process.argv[2]) {
   console.log('Missing number of occurrences');
-  return;
 }
 
 const count = parseInt(process.argv[2]);
 
-if (count < 1) return;
-
-for (let i = 0; i < count; i++) {
-  console.log('C is fun');
+if (count > 0) {
+  for (let i = 0; i < count; i++) {
+    console.log('C is fun');
+  }
 }
