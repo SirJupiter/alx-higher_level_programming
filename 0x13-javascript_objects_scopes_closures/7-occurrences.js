84bf15fff84863c4100f3425bc2f853acf591512
SirJupiter <jupitertoby@gmail.com>
2024-04-09 20:59:23 +0100

    7. Occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,11 +3,14 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
 
-  for (const element of list) {
-    if (element === searchElement) {
-      count++;
+  if (Array.isArray(list)) {
+    for (const element of list) {
+      if (element === searchElement) {
+        count++;
+      }
     }
   }
+
 
   return count;
 }
