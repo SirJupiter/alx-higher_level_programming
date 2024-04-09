5757396f2670e4a2e7209d2f8e085a37818355f5
SirJupiter <jupitertoby@gmail.com>
2024-04-09 22:05:14 +0100

    10. Number conversion

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,19 +2,6 @@
 
 exports.converter = function (base) {
   return function (number) {
-    const digits = '0123456789ABCDEF';
-
-    if (number === 0) {
-      return '0';
-    }
-
-    let result = '';
-    while (number > 0) {
-      const remainder = number % base;
-      result = digits[remainder] + result;
-      number = Math.floor(number / base);
-    }
-
-    return result;
+    return parseInt(number, base);
   };
 };
