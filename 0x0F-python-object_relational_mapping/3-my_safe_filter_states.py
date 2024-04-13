204722e1f36d921a18f02273c55774110eb4c643
SirJupiter <jupitertoby@gmail.com>
2024-04-13 13:16:56 +0100

    3. SQL Injection...

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """Main entry function"""
+
+    try:
+        db_config = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+
+        cursor = db_config.cursor()
+
+        query = "SELECT * FROM states WHERE name LIKE BINARY %s \
+            ORDER BY id ASC;"
+
+        try:
+            cursor.execute(query, [sys.argv[4]])
+        except MySQLdb.Error as err:
+            print("An error occurred: {}".format(err))
+        else:
+            results = cursor.fetchall()
+            for row in results:
+                print(row)
+
+    except MySQLdb.Error as err:
+        print("Error: Can't connect to Database ({})".format(err))
+
+    finally:
+        if cursor:
+            cursor.close()
+        if db_config:
+            db_config.close()
+
+
+if __name__ == "__main__":
+    main()
