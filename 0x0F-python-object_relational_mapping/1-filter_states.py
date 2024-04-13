f00cb5cc0f3f417d6625587844430aa8704329b0
SirJupiter <jupitertoby@gmail.com>
2024-04-13 12:28:18 +0100

    1. Filter states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""script that lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa"""
+
+import sys
+import MySQLdb
+
+
+def main():
+    """Entry point function"""
+
+    try:
+        db_config = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+
+        cursor = db_config.cursor()
+
+        query = "SELECT * FROM states WHERE name LIKE 'N%'"
+
+        try:
+            cursor.execute(query)
+        except MySQLdb.Error as e:
+            print("An error occurred: {}".format(e))
+        else:
+            result = cursor.fetchall()
+            for row in result:
+                print(row)
+
+    except MySQLdb.Error as err:
+        print("ERROR: Unable to connect to MySQL ({})".format(err))
+
+    finally:
+        if cursor:
+            cursor.close()
+        if db_config:
+            db_config.close()
+
+
+if __name__ == "__main__":
+    main()
