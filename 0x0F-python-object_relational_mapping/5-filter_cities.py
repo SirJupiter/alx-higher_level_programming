bd39a24bfea5462538161cc632d9e479db43fa5d
SirJupiter <jupitertoby@gmail.com>
2024-04-13 16:38:53 +0100

    5. All cities by state

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""script that takes in the name of a state as an argument and lists all
+cities of that state, using the database hbtn_0e_4_usa"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """Main entry function"""
+
+    try:
+        db_config = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
+
+        cursor = db_config.cursor()
+        state_name = sys.argv[4]
+
+        query = """
+        SELECT cities.name
+        FROM cities
+        INNER JOIN states ON cities.state_id = states.id
+        WHERE cities.name = %s
+        ORDER BY cities.id ASC;
+        """
+
+        try:
+            cursor.execute(query, (state_name,))
+        except MySQLdb.OperationalError as e:
+            print(f"An error occurred: {e}")
+        except Exception:
+            pass
+        else:
+            result = cursor.fetchall()
+            for row in result:
+                print(row)
+                if result[len(result) - 1] != row:
+                    print(row, end=', ')
+
+    except MySQLdb.Error as e:
+        print(f"Error: Could not connect to database ({e})")
+
+
+if __name__ == "__main__":
+    main()
