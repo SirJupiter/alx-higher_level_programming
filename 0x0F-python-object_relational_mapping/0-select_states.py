65ec42ef893b25f243be5a137b2d595b096f8452
SirJupiter <jupitertoby@gmail.com>
2024-04-13 12:03:20 +0100

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 """script that lists all states from the database hbtn_0e_0_usa"""
 
 import MySQLdb
@@ -7,26 +8,33 @@ import sys
 def main():
     """Main function"""
 
-    db_config = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
+    try:
+        db_config = MySQLdb.connect(
+            host='localhost',
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+        )
 
-    cursor = db_config.cursor()
-    query = "SELECT * FROM states ORDER BY id ASC;"
+        cursor = db_config.cursor()
+        query = "SELECT * FROM states ORDER BY id ASC;"
 
-    cursor.execute(query)
+        try:
+            cursor.execute(query)
+        except Exception as e:
+            print("Error executing query: {}".format(e))
+        else:
+            result = cursor.fetchall()
+            for row in result:
+                print(row)
 
-    result = cursor.fetchall()
-    for row in result:
-        print(row)
+    except MySQLdb.Error as err:
+        print("MySQL Error {:d}: {:s}".format(err.args[0], err.args[1]))
 
-    # Closing connection and cursor
-    cursor.close()
-    db_config.close()
+    finally:
+        # Closing connection and cursor
+        cursor.close()
+        db_config.close()
 
 
 if __name__ == "__main__":
