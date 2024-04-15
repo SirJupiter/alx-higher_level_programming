5a754c21db263a1e0ae5c5d2ea6f5aaf61e0869e
SirJupiter <jupitertoby@gmail.com>
2024-04-15 20:55:15 +0100

    9. Contains 'a'

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""script that lists all State objects that contain the letter a
+from the database hbtn_0e_6_usa"""
+
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}')
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).filter(
+        State.name.contains('a')).order_by(State.id).all()
+
+    for row in result:
+        print(f'{row.id}: {row.name}')
