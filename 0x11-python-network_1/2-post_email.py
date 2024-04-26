b68a1d591dd4ee16131b3e9453fc2c5587f90bfe
SirJupiter <jupitertoby@gmail.com>
2024-04-26 17:32:48 +0100

    2. POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,6 +4,7 @@ sends a POST request to the passed URL with the email as a parameter,
 and displays the body of the response (decoded in utf-8)
 """
 
+import urllib.error
 import urllib.request
 import urllib.parse
 from sys import argv
@@ -14,16 +15,22 @@ def main():
 
     url = argv[1]
 
+    data = {}
     if argv[2]:
         data = {'email': argv[2]}
-        encoded_data = urllib.parse.urlencode(data).encode('ascii')
 
-        request = urllib.request.Request(url, encoded_data)
+    encoded_data = urllib.parse.urlencode(data).encode('ascii')
+    request = urllib.request.Request(url, encoded_data)
+
+    try:
         with urllib.request.urlopen(request) as response:
             result = response.read()
             body = result.decode('utf-8')
 
-        print(f'Your email is: {body["email"]}')
+        print(body)
+
+    except urllib.error.URLError as e:
+        print(f"Error accessing {url}: {e}")
 
 
 if __name__ == "__main__":
