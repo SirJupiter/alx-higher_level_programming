a572a03ffb28eada3fae1494868ff220cb7e03eb
SirJupiter <jupitertoby@gmail.com>
2024-04-27 00:09:17 +0100

    6. POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""script takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response."""
+
+from sys import argv
+import requests
+
+
+def main():
+    """Main function"""
+
+    url = argv[1]
+
+    data = {}
+    if argv[2]:
+        data = {'email': argv[2]}
+
+    req = requests.post(url=url, params=data)
+
+    print(req)
+
+
+if __name__ == "__main__":
+    main()
