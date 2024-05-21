0f842e7ae958dc85ea69cc0da3856c7f19caf9bf
SirJupiter <jupitertoby@gmail.com>
2024-05-21 22:16:13 +0100

    2. Status code

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,11 +5,9 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const theString = process.argv[3];
 
-if (filePath && theString) {
-  fs.writeFile(filePath, theString, 'utf8', (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-  });
-}
+fs.writeFile(filePath, theString, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+});
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,7 +6,7 @@ const url = process.argv[2];
 
 request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error); 
+    console.error(error);
   } else {
     console.log('code: ', response.statusCode);
   }
