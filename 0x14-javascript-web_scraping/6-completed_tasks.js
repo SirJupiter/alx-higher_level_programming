f945e724c1930b4db5e524155f26fb35f81ffe62
SirJupiter <jupitertoby@gmail.com>
2024-05-22 09:39:58 +0100

    6. How many completed?

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,16 +4,16 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request.get(url, (error, response, body) => {
+request.get(url, {json: true}, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
     // console.log(body);
-    const data = JSON.parse(body);
+    // const data = JSON.parse(body);
 
     let result = {};
 
-    for (const item of data) {
+    for (const item of body) {
       if (item.completed) {
         if (!result[item.userId]) {
           result[item.userId] = 1;
