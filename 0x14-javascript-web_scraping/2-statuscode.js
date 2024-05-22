72f7e0ff2cc966701df1c46d0ed748f2904efae4
SirJupiter <jupitertoby@gmail.com>
2024-05-22 07:09:00 +0100

    2. Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,9 +4,9 @@ const request = require('request');
 
 const url = process.argv[2];
 
-request.get(url, (error, response) => {
+request.get(url, (error, response, body) => {
   if (error) {
-    console.error(error);
+    console.log(error);
   } else {
     console.log(`code: , ${response.statusCode}`);
   }
