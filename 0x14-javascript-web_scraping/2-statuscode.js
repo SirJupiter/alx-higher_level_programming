93fc331ee0aa0a2e8a36f716647edf175a4e1369
SirJupiter <jupitertoby@gmail.com>
2024-05-22 07:44:28 +0100

    3. Star wars movie title

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -9,6 +9,6 @@ request.get(url, (error, response) => {
   if (error) {
     console.log(error);
   } else {
-    console.log('code:' + ' ' + response.statusCode);
+    console.log(`code: ${response.statusCode}`);
   }
 });
