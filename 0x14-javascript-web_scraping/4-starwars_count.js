c27fe02f352b19d65f3c19b43c65f88bd9b66834
SirJupiter <jupitertoby@gmail.com>
2024-05-22 08:20:34 +0100

    4. Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,16 +4,18 @@ const request = require('request');
 
 const url = process.argv[2];
 
+let count = 0;
+
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
     const data = JSON.parse(body);
 
-    let count = 0;
-
     for (const movie of data.results) {
-      if (movie.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) count++;
+      for (const char of movie.characters) {
+        if (char.includes(18)) count++;
+      }
     }
 
     console.log(count);
